148eee722d2d471cf8127d40b720e0330e565d99
kimchchchchchche <codus618@naver.com>
2023-06-29 23:52:12 +0900

    초기설정

--- src/index.js
+++ src/index.js
@@ -3,12 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom'; // * BrowserRouter 불러오기
-import RandingPage from "./LandingPage";
-
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
 root.render(
   
   <BrowserRouter>
