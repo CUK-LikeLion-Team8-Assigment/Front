841755e3f0235a531a7f677cdc083f90f7581423
wkdrhkdwls <79461880+wkdrhkdwls@users.noreply.github.com>
2023-07-12 00:04:09 +0900

    Rename : Logout

--- src/components/Layout/sidebar.jsx
+++ src/components/Layout/sidebar.jsx
@@ -2,7 +2,7 @@ import React, { useRef, useEffect } from "react";
 import { FaTimes } from "react-icons/fa";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
-import LogoutButton from "../../pages/LogOut";
+import LogoutButton from "../LogOut";
 
 function Sidebar({ isOpen, setIsOpen }) {
   const outside = useRef(null);
