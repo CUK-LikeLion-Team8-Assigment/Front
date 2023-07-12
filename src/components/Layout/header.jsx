bdb8e2dd82b3c84d0e71090808fc15427fca9ead
wkdrhkdwls <79461880+wkdrhkdwls@users.noreply.github.com>
2023-07-12 21:08:17 +0900

    Design : 헤더 dark 삭제

--- src/components/Layout/header.jsx
+++ src/components/Layout/header.jsx
@@ -13,10 +13,10 @@ const Header = () => {
   };
 
   return (
-    <nav className="w-full top-0 left-0 z-50 border-gray-200 bg-gray-50 dark:bg-gray-800 dark:border-gray-700 xl:px-20 md:px-10">
+    <nav className="w-full top-0 left-0 z-50  bg-gray-800 border-gray-700 xl:px-20 md:px-10">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <Link to="/" className="flex items-center">
-          <span className="text-2xl font-semibold whitespace-nowrap dark:text-white">
+          <span className="text-2xl font-semibold whitespace-nowrap text-white">
             강의평가
           </span>
         </Link>
@@ -25,7 +25,7 @@ const Header = () => {
           <button
             onClick={toggleSide}
             type="button"
-            className="inline-flex items-center p-2 ml-3 text-sm text-black rounded-lg  focus:outline-none focus:ring-2 focus:ring-gray-200   dark:focus:ring-gray-600"
+            className="inline-flex items-center p-2 ml-3 text-sm text-black rounded-lg  focus:outline-none focus:ring-2 focus:ring-gray-600"
             aria-controls="navbar-hamburger"
             aria-expanded={isMenuOpen ? "true" : "false"}
           >
