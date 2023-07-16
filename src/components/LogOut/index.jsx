b4cc0797c6f35abf80b434d0e06d437b7796c72c
wkdrhkdwls <79461880+wkdrhkdwls@users.noreply.github.com>
2023-07-16 14:14:26 +0900

    logout change

--- src/components/LogOut/index.jsx
+++ src/components/LogOut/index.jsx
@@ -1,30 +1,32 @@
 import React from "react";
-import axios from "axios";
 import { useDispatch } from "react-redux";
 import { logout } from "../../actions/actions";
+import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
-const LogoutButton = () => {
+const Logout = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
-  const handleLogout = (e) => {
-    e.preventDefault();
-
+  const handleLogout = () => {
     axios
       .post("http://localhost:8080/logout")
       .then((response) => {
-        console.log(response.data);
+        console.log("로그아웃 성공", response.data);
         dispatch(logout());
         localStorage.setItem("isLoggedIn", "false");
         navigate("/login");
       })
       .catch((error) => {
-        console.error("Logout error:", error);
+        console.error("로그아웃 실패", error);
       });
   };
 
-  return <button onClick={handleLogout}>로그아웃</button>;
+  return (
+    <button onClick={handleLogout} className="text-blue-600 font-bold">
+      로그아웃
+    </button>
+  );
 };
 
-export default LogoutButton;
+export default Logout;
